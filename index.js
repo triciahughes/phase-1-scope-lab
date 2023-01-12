15cb2527d1d413e95555ddee223e8f5b7c722838
triciahughes <hughes.a.tricia@gmail.com>
2023-01-11 19:13:45 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "karen";
+
+var upperCaseCustomerName = () => {
+  customerName = "BOB";
+};
+
+var setBestCustomer = () => {
+  bestCustomer = "not bob";
+};
+
+var overwriteBestCustomer = () => {
+  bestCustomer = "maybe bob";
+};
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "mary";
+}
